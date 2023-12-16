cfc371d5202e80e4e659cfb7f6038b2e348c7d8d
Geraldo Pereira <110185110+Geraldopereirads@users.noreply.github.com>
2023-12-16 08:27:02 -0300

    feat: middlewares add

---
+++ src/middlewares/isAdmin.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/App.error";
+
+export const isAdmin = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const admin: boolean = res.locals.decoded.admin;
+
+  if (!admin) throw new AppError("Insufficient permissions", 403);
+
+  return next();
+};
---
+++ src/middlewares/verifyToken.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/App.error";
+import { verify } from "jsonwebtoken";
+
+export const verifyTokenM = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const authorization: string | undefined = req.headers.authorization;
+
+  if (!authorization) throw new AppError("Missing bearer token", 401);
+
+  const [_bearer, token]: Array<string> = authorization.split(" ");
+
+  res.locals = {
+    ...res.locals,
+    decoded: verify(token, process.env.SECRET_KEY!),
+  };
+
+  return next();
+};
