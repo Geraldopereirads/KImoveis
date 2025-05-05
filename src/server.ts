c9b1fd2235b530c8e5d1d0fbc513183a726c84f2
Geraldopereirads <geraldo.pereirads@hotmail.com>
2025-05-05 19:05:27 -0300

    fix: repair server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import app from "./app";
 import { AppDataSource } from "./data-source";
 
@@ -7,7 +8,19 @@ AppDataSource.initialize()
 
     const PORT: number = Number(process.env.PORT) || 3000;
 
-    app.listen(PORT, () => console.log("Servidor executando"));
+    app.listen(PORT, () => {
+      console.log("Servidor executando");
+
+      setInterval(async () => {
+        try {
+          const url = "https://seu-backend-no-render.onrender.com";
+          const res = await axios.get(url);
+          console.log(`Auto-ping OK: ${res.status}`);
+        } catch (error: any) {
+          console.error("Erro no auto-ping:", error.message);
+        }
+      }, 5 * 60 * 1000);
+    });
   })
   .catch((err: unknown): void => {
     console.error("Error during Data Source initialization", err);
